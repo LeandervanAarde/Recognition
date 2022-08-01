42266bdf282045e4f736bb2297eb4a4834399ceb
LeandervanAarde <200211@virtualwindow.co.za>
2022-08-01 15:10:04 +0200

    First page Routing is done
    First page can now Route to the home screen

--- app/src/main/java/com/example/recognition/MainActivity.kt
+++ app/src/main/java/com/example/recognition/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.recognition
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonClick = findViewById<com.google.android.material.button.MaterialButton>(R.id.btn_start)
+        buttonClick.setOnClickListener{
+            Log.i("Clicked?", "YES")
+            val intent = Intent(this, HomeScreen::class.java )
+            startActivity(intent)
+        }
     }
 }
