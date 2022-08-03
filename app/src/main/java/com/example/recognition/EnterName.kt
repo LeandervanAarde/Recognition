3cb8e6df67cdc0b892b3fe7df39dd3ee99e1323b
LeandervanAarde <200211@virtualwindow.co.za>
2022-08-03 23:55:54 +0200

    Settings added
    Settings page has been added

--- app/src/main/java/com/example/recognition/EnterName.kt
+++ app/src/main/java/com/example/recognition/EnterName.kt
@@ -2,10 +2,15 @@ package com.example.recognition
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.recognition.databinding.ActivityEnterNameBinding
 
 class EnterName : AppCompatActivity() {
+    private lateinit var binding: ActivityEnterNameBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_enter_name)
+        binding = ActivityEnterNameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
